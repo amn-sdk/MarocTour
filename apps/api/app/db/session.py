70d1a45e6f0ff4d2e789115cdac6e53d6a27116b
amn-sdk <amine.saddik@edu.esiee.fr>
2025-12-11 13:07:44 +0100

    ci: fix test working on Github Action CI backend v 0.3

--- apps/api/app/db/session.py
+++ apps/api/app/db/session.py
@@ -6,12 +6,21 @@ from sqlmodel import Session, create_engine
 from app.core.config import settings
 
 # Create database engine
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_size=settings.DATABASE_POOL_SIZE,
-    max_overflow=settings.DATABASE_MAX_OVERFLOW,
-    echo=settings.DEBUG,
-)
+# Create database engine
+if settings.DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(
+        settings.DATABASE_URL,
+        echo=settings.DEBUG,
+        connect_args=connect_args,
+    )
+else:
+    engine = create_engine(
+        settings.DATABASE_URL,
+        pool_size=settings.DATABASE_POOL_SIZE,
+        max_overflow=settings.DATABASE_MAX_OVERFLOW,
+        echo=settings.DEBUG,
+    )
 
 
 def get_session():
